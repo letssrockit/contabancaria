c5d572a902eaaf19fff52beece5a21e6db945ec3
letssrockit <leticiarocha_22@outlook.com.br>
2023-12-19 18:02:31 -0300

    Adição de interface

---
+++ conta/src/conta/Menu.java
@@ -0,0 +1,149 @@
+package conta;
+
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.Scanner;
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+import conta.util.Cores;
+
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		
+		//teste classe conta corrente
+		
+		Conta cc1 = new ContaCorrente (2,231,1, "Mariana", 15000.0f,1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		//teste conta poupança
+		
+		Conta cp1 = new ContaPoupanca  (3,123,2,"Victor",100000.0f,15);
+		cp1.visualizar();
+		cp1.sacar(1000);
+		cp1.visualizar();
+		cp1.depositar(5000);
+		cp1.visualizar();
+		
+		
+		Scanner leia = new Scanner (System.in);
+		
+		int opcao;
+		
+		while (true) {
+			
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND);
+			
+			System.out.println("************************************************");
+			System.out.println("                                                ");
+			System.out.println("            BANCO DO BRAZIL COM Z               ");
+			System.out.println("                                                ");
+			System.out.println("************************************************");
+			System.out.println("                                                ");
+			System.out.println("          1 - Criar conta                       ");
+			System.out.println("          2 - Listar todas as contas            ");
+			System.out.println("          3 - Buscar conta por número           ");
+			System.out.println("          4 - Atualizar dados da conta          ");
+			System.out.println("          5 - Apagar conta                      ");
+			System.out.println("          6 - Sacar                             ");
+			System.out.println("          7- Depositar                          ");
+			System.out.println("          8- Transferir valores entre contas    ");
+			System.out.println("          9 - Sair                              ");
+			System.out.println("                                                ");
+			System.out.println("************************************************");
+			System.out.println("\nEntre com a opção desejada:                   ");
+			System.out.println("                                                "+ Cores.TEXT_RESET);
+			
+			try {
+				opcao = leia.nextInt();
+				}catch(InputMismatchException e){
+					System.out.println("\nDigite valores inteiros!");
+					leia.nextLine();
+					opcao=0;	
+		}
+			
+			if (opcao==9) {
+				System.out.println(Cores.TEXT_WHITE_BOLD +"\nBanco do Brazil com Z - Seu futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+			
+			switch(opcao) {
+			
+			case 1:
+				System.out.println(Cores.TEXT_WHITE_BOLD +"Criar conta \n\n");
+				break;
+				
+			case 2:
+				System.out.println(Cores.TEXT_WHITE_BOLD +"Listar todas as contas\n\n");
+				
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE_BOLD +"Consultar dados da conta - por número\n\n");
+				
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE_BOLD +"Atualizar dados da conta\n\n");
+				
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE_BOLD +"Apagar a conta\n\n");
+				
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE_BOLD +"Saque\n\n");
+				
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE_BOLD +"Depósito\n\n");
+				
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE_BOLD +"Transferência entre contas\n\n");
+				
+				break;
+				
+				default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção inválida\n");
+				break;
+			}
+		}
+			
+		
+		
+		
+
+	}
+
+	private static void sobre() {
+		System.out.println("************************************************");
+		System.out.println("Projeto desenvolvido por: Leticia Santana Rocha");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("https://github.com/letssrockit");
+		System.out.println("************************************************");
+		
+		
+	}
+	
+	public static void keyPress() {
+
+		try {
+
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
+	}
+}
